3657a30ed9dcafabb70a921eefd18f0e0a60bee2
Gallardo994 <evolutionv8@yandex.ru>
2024-01-07 05:25:55 +0400

    Make internal stuff private

--- imkcpp/include/imkcpp.hpp
+++ imkcpp/include/imkcpp.hpp
@@ -66,6 +66,15 @@ namespace imkcpp {
         std::function<i32(std::span<const std::byte> data, const ImKcpp& imkcpp, std::optional<void*> user)> output;
 
         void call_output(const std::span<const std::byte>& data) const;
+        void update_ack(i32 rtt);
+        void shrink_buf();
+        void parse_ack(u32 sn);
+        void parse_una(u32 una);
+        void parse_fastack(u32 sn, u32 ts);
+        void ack_push(u32 sn, u32 ts);
+        [[nodiscard]] std::optional<Ack> ack_get(size_t p) const;
+        void parse_data(const segment& newseg);
+        [[nodiscard]] i32 wnd_unused() const;
 
     public:
         explicit ImKcpp(u32 conv, std::optional<void*> user = std::nullopt);
@@ -74,25 +83,17 @@ namespace imkcpp {
         void set_interval(u32 interval);
         void set_nodelay(i32 nodelay, u32 interval, i32 resend, i32 nc);
         void set_mtu(u32 mtu);
-        [[nodiscard]] u32 get_mtu() const;
-        [[nodiscard]] u32 get_max_segment_size() const;
         void set_wndsize(u32 sndwnd, u32 rcvwnd);
 
-        [[nodiscard]] i32 peek_size() const;
-        void update_ack(i32 rtt);
-        void shrink_buf();
-        void parse_ack(u32 sn);
-        void parse_una(u32 una);
-        void parse_fastack(u32 sn, u32 ts);
-        void ack_push(u32 sn, u32 ts);
-        [[nodiscard]] std::optional<Ack> ack_get(size_t p) const;
         i32 recv(std::span<std::byte>& buffer);
         i32 send(const std::span<const std::byte>& buffer);
-        void parse_data(const segment& newseg);
         i32 input(const std::span<const std::byte>& data);
         void update(u32 current);
-        void flush();
         u32 check(u32 current);
-        [[nodiscard]] i32 wnd_unused() const;
+        void flush();
+
+        [[nodiscard]] u32 get_mtu() const;
+        [[nodiscard]] u32 get_max_segment_size() const;
+        [[nodiscard]] i32 peek_size() const;
     };
 }
