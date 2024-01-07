0362ad7bb423b5ce34028dfcce5987602e8cf7a4
Gallardo994 <evolutionv8@yandex.ru>
2024-01-07 04:16:53 +0400

    Update Initialization_Tests.cpp

--- tests/Initialization_Tests.cpp
+++ tests/Initialization_Tests.cpp
@@ -11,3 +11,14 @@ TEST(Initialization_Tests, Mtu_Sets) {
     EXPECT_EQ(kcp.get_mtu(), 128);
     EXPECT_EQ(kcp.get_max_segment_size(), 128 - IKCP_OVERHEAD);
 }
+
+TEST(Initialization_Tests, Mtu_InvalidValues) {
+    imkcpp kcp(0);
+
+    for (u32 i = 0; i < IKCP_OVERHEAD; ++i) {
+        kcp.set_mtu(i);
+
+        EXPECT_EQ(kcp.get_mtu(), IKCP_MTU_DEF);
+        EXPECT_EQ(kcp.get_max_segment_size(), IKCP_MTU_DEF - IKCP_OVERHEAD);
+    }
+}
