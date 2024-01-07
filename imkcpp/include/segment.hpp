a361608a27ccc749baddb345ac567b549e225dd8
Gallardo994 <evolutionv8@yandex.ru>
2024-01-07 05:47:43 +0400

    Update segment.hpp

--- imkcpp/include/segment.hpp
+++ imkcpp/include/segment.hpp
@@ -6,24 +6,21 @@
 
 namespace imkcpp {
     struct segment {
-        u32 conv, cmd, frg;
-        u32 wnd, ts, sn, una;
-        u32 resendts, rto;
-        u32 fastack, xmit;
-        std::vector<std::byte> data;
+        u32 conv = 0;
+        u32 cmd = 0;
+        u32 frg = 0;
+        u32 wnd = 0;
+        u32 ts = 0;
+        u32 sn = 0;
+        u32 una = 0;
+        u32 resendts = 0;
+        u32 rto = 0;
+        u32 fastack = 0;
+        u32 xmit = 0;
+        std::vector<std::byte> data{};
 
-        explicit segment() : conv(0), cmd(0), frg(0),
-                             wnd(0), ts(0), sn(0), una(0),
-                             resendts(0), rto(0),
-                             fastack(0), xmit(0) {
-        }
-
-        explicit segment(const size_t size) : conv(0), cmd(0), frg(0),
-                                              wnd(0), ts(0), sn(0), una(0),
-                                              resendts(0), rto(0),
-                                              fastack(0), xmit(0),
-                                              data(size) {
-        }
+        explicit segment() = default;
+        explicit segment(const size_t size) : data(size) { }
 
         void encode_to(std::vector<std::byte>& buf) const {
             assert(buf.size() >= constants::IKCP_OVERHEAD);
