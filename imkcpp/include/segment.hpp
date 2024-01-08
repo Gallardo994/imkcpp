8909fdf3f2acd0823b74d230f76e7168fa455dae
Gallardo994 <evolutionv8@yandex.ru>
2024-01-09 02:06:21 +0400

    Update segment.hpp

--- imkcpp/include/segment.hpp
+++ imkcpp/include/segment.hpp
@@ -18,27 +18,27 @@ namespace imkcpp {
         void encode_to(std::span<std::byte>& buf, size_t& offset) const {
             assert(buf.size() >= constants::IKCP_OVERHEAD);
 
-            encoder::encode32u(buf, offset, conv);
-            encoder::encode8u(buf, offset, cmd);
-            encoder::encode8u(buf, offset, frg);
-            encoder::encode16u(buf, offset, wnd);
-            encoder::encode32u(buf, offset, ts);
-            encoder::encode32u(buf, offset, sn);
-            encoder::encode32u(buf, offset, una);
-            encoder::encode32u(buf, offset, len);
+            encoder::encode32u(buf, offset, this->conv);
+            encoder::encode8u(buf, offset, this->cmd);
+            encoder::encode8u(buf, offset, this->frg);
+            encoder::encode16u(buf, offset, this->wnd);
+            encoder::encode32u(buf, offset, this->ts);
+            encoder::encode32u(buf, offset, this->sn);
+            encoder::encode32u(buf, offset, this->una);
+            encoder::encode32u(buf, offset, this->len);
         }
 
         void decode_from(const std::span<const std::byte> buf, size_t& offset) {
             assert(buf.size() >= constants::IKCP_OVERHEAD);
 
-            encoder::decode32u(buf, offset, conv);
-            encoder::decode8u(buf, offset, cmd);
-            encoder::decode8u(buf, offset, frg);
-            encoder::decode16u(buf, offset, wnd);
-            encoder::decode32u(buf, offset, ts);
-            encoder::decode32u(buf, offset, sn);
-            encoder::decode32u(buf, offset, una);
-            encoder::decode32u(buf, offset, len);
+            encoder::decode32u(buf, offset, this->conv);
+            encoder::decode8u(buf, offset, this->cmd);
+            encoder::decode8u(buf, offset, this->frg);
+            encoder::decode16u(buf, offset, this->wnd);
+            encoder::decode32u(buf, offset, this->ts);
+            encoder::decode32u(buf, offset, this->sn);
+            encoder::decode32u(buf, offset, this->una);
+            encoder::decode32u(buf, offset, this->len);
         }
     };
 
@@ -56,19 +56,19 @@ namespace imkcpp {
         explicit SegmentData(const size_t size) : data(size) { }
 
         void assign(const std::span<const std::byte> buf) {
-            data.assign(buf.begin(), buf.end());
+            this->data.assign(buf.begin(), buf.end());
         }
 
         void encode_to(std::span<std::byte>& buf, size_t& offset, const size_t length) const {
-            assert(buf.size() >= data.size());
+            assert(buf.size() >= this->data.size());
 
-            std::memcpy(buf.data() + offset, data.data(), length);
+            std::memcpy(buf.data() + offset, this->data.data(), length);
             offset += length;
         }
 
         void decode_from(const std::span<const std::byte> buf, size_t& offset, const size_t length) {
             assert(buf.size() >= offset + length);
-            assign(buf.subspan(offset, length));
+            this->assign(buf.subspan(offset, length));
             offset += length;
         }
     };
@@ -82,30 +82,30 @@ namespace imkcpp {
 
         void encode_to(std::span<std::byte> buf, size_t& offset) const {
             assert(buf.size() >= constants::IKCP_OVERHEAD + header.len);
-            assert(header.len == data.data.size());
+            assert(this->header.len == this->data.data.size());
 
-            header.encode_to(buf, offset);
-            data.encode_to(buf, offset, header.len);
+            this->header.encode_to(buf, offset);
+            this->data.encode_to(buf, offset, this->header.len);
         }
 
         void decode_from(const std::span<const std::byte> buf, size_t& offset) {
-            assert(buf.size() >= constants::IKCP_OVERHEAD + header.len);
+            assert(buf.size() >= constants::IKCP_OVERHEAD + this->header.len);
 
-            header.decode_from(buf, offset);
-            data.decode_from(buf, offset, header.len);
+            this->header.decode_from(buf, offset);
+            this->data.decode_from(buf, offset, this->header.len);
         }
 
         [[nodiscard]] size_t size() const {
-            return constants::IKCP_OVERHEAD + header.len;
+            return constants::IKCP_OVERHEAD + this->data_size();
         }
 
         [[nodiscard]] size_t data_size() const {
-            return header.len;
+            return this->header.len;
         }
 
         void data_assign(const std::span<const std::byte> buf) {
-            data.assign(buf);
-            header.len = buf.size();
+            this->data.assign(buf);
+            this->header.len = buf.size();
         }
     };
 }
