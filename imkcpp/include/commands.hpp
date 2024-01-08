50af7303f65c26e26d471d7aae36e7c10bf4ba9e
Gallardo994 <evolutionv8@yandex.ru>
2024-01-09 01:39:17 +0400

    Update commands.hpp

--- imkcpp/include/commands.hpp
+++ imkcpp/include/commands.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
-namespace imkcpp::commands {
-    constexpr uint32_t IKCP_CMD_PUSH = 81;
-    constexpr uint32_t IKCP_CMD_ACK  = 82;
-    constexpr uint32_t IKCP_CMD_WASK = 83;
-    constexpr uint32_t IKCP_CMD_WINS = 84;
+#include "types.hpp"
 
-    inline bool is_valid(const uint32_t cmd) {
+namespace imkcpp::commands {
+    constexpr u8 IKCP_CMD_PUSH = 81;
+    constexpr u8 IKCP_CMD_ACK  = 82;
+    constexpr u8 IKCP_CMD_WASK = 83;
+    constexpr u8 IKCP_CMD_WINS = 84;
+
+    inline bool is_valid(const u8 cmd) {
         return cmd >= IKCP_CMD_PUSH && cmd <= IKCP_CMD_WINS;
     }
 }
