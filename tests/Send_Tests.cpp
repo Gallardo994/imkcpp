8a25366b0a96dd776f9c8c823f1cf83494654139
Gallardo994 <evolutionv8@yandex.ru>
2024-01-09 02:27:00 +0400

    Update Send_Tests.cpp

--- tests/Send_Tests.cpp
+++ tests/Send_Tests.cpp
@@ -121,24 +121,13 @@ TEST(Send_Tests, Send_ZeroBytes) {
     EXPECT_EQ(result.error(), error::buffer_too_small);
 }
 
-TEST(Send_Tests, Send_TooManyFragments) {
-    using namespace imkcpp;
-
-    ImKcpp kcp(0);
-    kcp.set_wndsize(2048, 2048);
-
-    std::vector<std::byte> data(kcp.get_max_segment_size() * 255 + 1);
-    auto result = kcp.send(data);
-    EXPECT_EQ(result.error(), error::too_many_fragments);
-}
-
 TEST(Send_Tests, Send_ExceedsWindowSize) {
     using namespace imkcpp;
 
     ImKcpp kcp(0);
     kcp.set_wndsize(128, 128);
 
-    std::vector<std::byte> data(kcp.get_max_segment_size() * 255);
+    std::vector<std::byte> data(kcp.get_max_segment_size() * 128 + 1);
     auto result = kcp.send(data);
     EXPECT_EQ(result.error(), error::exceeds_window_size);
 }
