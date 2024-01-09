9c64fd37223e72153691cb9b704d534a95a83b71
Gallardo994 <evolutionv8@yandex.ru>
2024-01-10 00:56:30 +0400

    Simplify reading the header

--- imkcpp/include/imkcpp.hpp
+++ imkcpp/include/imkcpp.hpp
@@ -38,39 +38,36 @@ namespace imkcpp {
         Receiver receiver{congestion_controller, shared_ctx};
         Sender sender{congestion_controller, rto_calculator, flusher, ack_controller, shared_ctx};
 
-        // TODO: This needs to be split into receiver and sender, and maybe shared context part
-
-        // TODO: And for the god's sake, rename all these variables to something more meaningful
-
+        bool updated = false;
         u32 current = 0;
         u32 interval = constants::IKCP_INTERVAL;
         u32 ts_flush = constants::IKCP_INTERVAL;
 
-        bool updated = false;
-
-        [[nodiscard]] Segment create_service_segment(i32 unused_receive_window) const;
+        [[nodiscard]] auto create_service_segment(i32 unused_receive_window) const -> Segment;
 
     public:
         explicit ImKcpp(u32 conv);
 
-        [[nodiscard]] State get_state() const;
-        void set_interval(u32 interval);
-        void set_nodelay(i32 nodelay);
-        void set_resend(i32 resend);
-        tl::expected<size_t, error> set_mtu(u32 mtu);
-        void set_wndsize(u32 sndwnd, u32 rcvwnd);
-        void set_congestion_window_enabled(bool state);
+        auto set_interval(u32 interval) -> void;
+        auto set_nodelay(i32 nodelay) -> void;
+        auto set_resend(i32 resend) -> void;
+        auto set_mtu(u32 mtu) -> tl::expected<size_t, error>;
+        auto set_wndsize(u32 sndwnd, u32 rcvwnd) -> void;
+        auto set_congestion_window_enabled(bool state) -> void;
 
-        tl::expected<size_t, error> recv(std::span<std::byte> buffer);
-        [[nodiscard]] size_t estimate_segments_count(size_t size) const;
-        tl::expected<size_t, error> send(std::span<const std::byte> buffer);
-        tl::expected<size_t, error> input(std::span<const std::byte> data);
-        FlushResult update(u32 current, const output_callback_t& callback);
-        u32 check(u32 current);
-        FlushResult flush(const output_callback_t& callback);
+        auto input(std::span<const std::byte> data) -> tl::expected<size_t, error>;
+        auto recv(std::span<std::byte> buffer) -> tl::expected<size_t, error>;
 
-        [[nodiscard]] u32 get_mtu() const;
-        [[nodiscard]] u32 get_max_segment_size() const;
-        [[nodiscard]] tl::expected<size_t, error> peek_size() const;
+        auto send(std::span<const std::byte> buffer) -> tl::expected<size_t, error>;
+        auto check(u32 current) -> u32;
+        auto update(u32 current, const output_callback_t& callback) -> FlushResult;
+        auto flush(const output_callback_t& callback) -> FlushResult;
+
+        [[nodiscard]] auto get_state() const -> State;
+        [[nodiscard]] auto get_mtu() const -> u32;
+        [[nodiscard]] auto get_max_segment_size() const -> u32;
+
+        [[nodiscard]] auto peek_size() const -> tl::expected<size_t, error>;
+        [[nodiscard]] auto estimate_segments_count(size_t size) const -> size_t;
     };
 }
