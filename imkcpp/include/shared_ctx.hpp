6d9838c931d298ce87b01386bfcbbb3de3eb7ba5
Gallardo994 <evolutionv8@yandex.ru>
2024-01-10 20:55:51 +0400

    More comments

--- imkcpp/include/shared_ctx.hpp
+++ imkcpp/include/shared_ctx.hpp
@@ -8,20 +8,22 @@ namespace imkcpp {
 
     public:
         // TODO: Make private and add meaningful getters/setters
-        u32 conv = 0;
-        size_t mtu = 0;
-        size_t mss = 0;
+        u32 conv = 0; // Conversation ID
+        size_t mtu = 0; // Maximum Transmission Unit
+        size_t mss = 0; // Maximum Segment Size (mtu - header size)
 
-        u32 snd_una = 0;
-        u32 snd_nxt = 0;
-        u32 rcv_nxt = 0;
+        u32 snd_una = 0; // Sequence number of the first unacknowledged segment.
+        u32 snd_nxt = 0; // Sequence number of the next segment to be sent.
+        u32 rcv_nxt = 0; // Sequence number of the next segment to be received.
 
-        u32 interval = constants::IKCP_INTERVAL;
+        u32 interval = constants::IKCP_INTERVAL; // Interval.
 
+        // Gets the current state of the connection.
         [[nodiscard]] State get_state() const {
             return this->state;
         }
 
+        // Sets the current state of the connection.
         void set_state(const State state) {
             this->state = state;
         }
--- imkcpp/include/utility.hpp
+++ imkcpp/include/utility.hpp
@@ -3,6 +3,7 @@
 #include "types.hpp"
 
 namespace imkcpp {
+    // Returns i32 value of the difference between two u32 values, aka a - b
     static i32 time_delta(const u32 later, const u32 earlier) {
         return static_cast<i32>(later - earlier);
     }
