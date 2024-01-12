926161fa8bc4115f2d0770016fbebf3ea378a3af
Gallardo994 <evolutionv8@yandex.ru>
2024-01-12 15:33:41 +0400

    Update congestion_controller.hpp

--- imkcpp/include/congestion_controller.hpp
+++ imkcpp/include/congestion_controller.hpp
@@ -74,6 +74,7 @@ namespace imkcpp {
             return this->snd_wnd;
         }
 
+        // Closely resembles TCP Reno algorithm.
         void packets_resent(const u32 packets_in_flight, const u32 resent) {
             this->ssthresh = std::max(packets_in_flight / 2, constants::IKCP_THRESH_MIN);
             this->cwnd = this->ssthresh + resent;
